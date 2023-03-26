c4bfb229f6c87968510fee87108b44492503f63c
Hernan Gurmendi <hgurmendi@kindalab.co>
2023-03-26 19:46:33 -0300

    WIP: improve docker interaction and fix segmentation fault when printing command

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -3,7 +3,15 @@
 # We want to exit the script if any of the commands fail.
 set -e
 
+# Build the container image and tag it "soi-memcached".
+echo "Building image..."
 docker build -t soi-memcached .
+
+# Remove the container with name "memcached" if it exists.
+echo "Removing existing container, if any..."
+docker rm -f memcached
+
 # We have to pass --init so that signals are forwarded to the container so we can use
 # CTRL+C to stop the process.
+echo "Running server..."
 docker run --init -p 7666:7666 -p 7667:7667 --rm -it --name memcached soi-memcached
--- src/protocol/command.c
+++ src/protocol/command.c
@@ -60,6 +60,14 @@ void command_print(struct Command *command) {
   printf("Command:\n");
   printf("Type: %s (%d)\n", binary_type_str(command->type), command->type);
   // TODO: Improve printing because BoundedData->data might not be printable.
-  printf("Arg1 (size %ld): %s\n", command->arg1->size, command->arg1->data);
-  printf("Arg2 (size %ld): %s\n", command->arg2->size, command->arg2->data);
+  if (command->arg1 != NULL) {
+    printf("Arg1 (size %ld): %s\n", command->arg1->size, command->arg1->data);
+  } else {
+    printf("Arg1: <nothing>\n");
+  }
+  if (command->arg2 != NULL) {
+    printf("Arg2 (size %ld): %s\n", command->arg2->size, command->arg2->data);
+  } else {
+    printf("Arg2: <nothing>\n");
+  }
 }
