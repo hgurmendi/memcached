d409f22a2c203ac7cacc7c29506aa33bdcad4d21
Hernan Gurmendi <hgurmendi@kindalab.co>
2023-03-26 06:52:19 -0300

    WIP: binary protocol adapted to bounded data

--- src/protocol/binary.c
+++ src/protocol/binary.c
@@ -3,41 +3,39 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
+#include <unistd.h>
 
 #include "binary.h"
 
 /* Reads an argument from the binary client according to the text protocol
- * specification.
- * Allocates memory for the buffer where the binary argument is going to be
- * stored and stores it in the `arg` pointer, and stores the size in `arg_size`.
- * Returns true if it was able to successfully read the argument from the
- * buffer, false otherwise.
+ * specification and returns a BoundedData instance. If at any point the binary
+ * protocol is broken a NULL pointer is returned, otherwise a pointer to a
+ * BoundedData instance with the argument and its size is returned.
  */
-static bool read_argument_from_binary_client(int client_fd, uint32_t *arg_size,
-                                             char **arg) {
+static struct BoundedData *read_argument_from_binary_client(int client_fd) {
   int bytes_read;
+  uint32_t arg_size;
 
-  bytes_read = recv(client_fd, (void *)arg_size, 4, 0);
-  if (bytes_read < 0) {
-    perror("Error reading argument size from binary client.");
-    return false;
+  bytes_read = read(client_fd, &arg_size, sizeof(uint32_t));
+  if (bytes_read != sizeof(uint32_t)) {
+    perror("read_argument_from_binary_client read 1");
+    return NULL;
   }
 
-  *arg_size = ntohl(*arg_size);
-  *arg = calloc(*arg_size, sizeof(char));
-  if (*arg == NULL) {
-    perror("Error allocating memory for binary client argument");
-    return false;
+  char *arg = malloc(arg_size);
+  if (arg == NULL) {
+    perror("read_argument_from_binary_client malloc");
+    return NULL;
   }
 
-  bytes_read = recv(client_fd, (void *)*arg, *arg_size, 0);
-  if (bytes_read < 0) {
-    perror("Error reading argument from binary client.");
-    return false;
+  bytes_read = read(client_fd, arg, arg_size);
+  if (bytes_read != arg_size) {
+    perror("read_argument_from_binary_client read 2");
+    free(arg);
+    return NULL;
   }
 
-  return true;
+  return bounded_data_create_from_buffer(arg, arg_size);
 }
 
 /* Reads a command from a client that is communicating using the binary
@@ -46,90 +44,92 @@ static bool read_argument_from_binary_client(int client_fd, uint32_t *arg_size,
  * given command, if any of them is not NULL.
  */
 void read_command_from_binary_client(int client_fd, struct Command *command) {
-  int bytes_read = recv(client_fd, &command->type, 1, 0);
-  if (bytes_read < 0) {
-    perror("Error reading command type from binary client");
+  int command_type = 0;
+  int bytes_read = read(client_fd, &command_type, 1);
+  if (bytes_read == -1) {
+    perror("read_command_from_binary_client read");
     command->type = BT_EINVAL;
     return;
   }
 
+  command->type = command_type;
+
   switch (command->type) {
   case BT_PUT:
-    if (!read_argument_from_binary_client(client_fd, &command->arg1_size,
-                                          &command->arg1) ||
-        !read_argument_from_binary_client(client_fd, &command->arg2_size,
-                                          &command->arg2)) {
+    command->arg1 = read_argument_from_binary_client(client_fd);
+    if (command->arg1 == NULL) {
       command->type = BT_EINVAL;
+      break;
+    }
+    command->arg2 = read_argument_from_binary_client(client_fd);
+    if (command->arg2 == NULL) {
+      // Clear the previous command
       command_destroy_args(command);
+      command->type = BT_EINVAL;
+      break;
     }
     break;
   case BT_DEL:
-    if (!read_argument_from_binary_client(client_fd, &command->arg1_size,
-                                          &command->arg1)) {
-      command->type = BT_EINVAL;
-      command_destroy_args(command);
-    }
-    break;
   case BT_GET:
-    if (!read_argument_from_binary_client(client_fd, &command->arg1_size,
-                                          &command->arg1)) {
-      command->type = BT_EINVAL;
-      command_destroy_args(command);
-    }
-    break;
   case BT_TAKE:
-    if (!read_argument_from_binary_client(client_fd, &command->arg1_size,
-                                          &command->arg1)) {
+    command->arg1 = read_argument_from_binary_client(client_fd);
+    if (command->arg1 == NULL) {
       command->type = BT_EINVAL;
-      command_destroy_args(command);
+      break;
     }
     break;
   case BT_STATS:
-    break;
   default:
     break;
   }
 }
 
 /* Writes a command to a client that is communicating using the binary protocol.
- * Each response of the server is encoded in a Command structure, where the
- * command's `type` member determines the first token of the response the
- * command's `arg1` member (along with `arg1_size`) determine an optional
- * argument.
+ * Each response is encoded in a `Command` structure, where the `arg1` member
+ * represents the optional payload (`NULL` means no payload). The payload is
+ * only valid when sending a BT_OK response.
  * Returns -1 if something wrong happens, 0 otherwise.
  */
 int write_command_to_binary_client(int client_fd,
                                    struct Command *response_command) {
-  char response_code;
-  int status;
+  char response_code = response_command->type;
+  int bytes_written;
 
   // These are the only valid options for the binary client.
-  if (response_command->type != BT_EINVAL || response_command->type != BT_OK ||
-      response_command->type != BT_ENOTFOUND) {
+  if (response_code != BT_EINVAL || response_code != BT_OK ||
+      response_code != BT_ENOTFOUND) {
+    fprintf(stderr, "Invalid response code in binary protocol response");
     return -1;
   }
 
-  status = send(client_fd, &response_code, 1, 0);
-  if (status < 0) {
-    perror("Error sending data to binary client");
+  bytes_written = write(client_fd, &response_code, 1);
+  if (bytes_written != 1) {
+    perror("write_command_to_binary_client write 1");
     return -1;
   }
 
+  // No argument with the command, the response has been already sent.
   if (response_command->arg1 == NULL) {
     return 0;
   }
 
-  uint32_t marshalled_size = htonl(response_command->arg1_size);
-  status = send(client_fd, &marshalled_size, sizeof(uint32_t), 0);
-  if (status < 0) {
-    perror("Error sending data to binary client");
+  if (response_command->type != BT_OK) {
+    fprintf(stderr,
+            "Trying to send an argument to a response different from BT_OK");
     return -1;
   }
 
-  status =
-      send(client_fd, response_command->arg1, response_command->arg1_size, 0);
-  if (status < 0) {
-    perror("Error sending data to binary client");
+  uint32_t arg_size = response_command->arg1->size;
+  uint32_t marshalled_size = htonl(arg_size);
+  bytes_written = write(client_fd, &marshalled_size, sizeof(uint32_t));
+  if (bytes_written != sizeof(uint32_t)) {
+    perror("write_command_to_binary_client write 2");
+    return -1;
+  }
+
+  bytes_written = write(client_fd, response_command->arg1->data, arg_size);
+  if (bytes_written != arg_size) {
+    perror("write_command_to_binary_client write 3");
     return -1;
   }
 
