a31156e292cd00da749107acb6d66d74cfd14364
Hernan Gurmendi <hgurmendi@kindalab.co>
2023-04-04 15:04:44 -0300

    Remove more unused code

--- src/bounded_data.c
+++ src/bounded_data.c
@@ -16,28 +16,6 @@ bool bounded_data_equals(struct BoundedData *a, struct BoundedData *b) {
   return memcmp(a->data, b->data, a->size) == 0;
 }
 
-// Allocates memory for a copy of the given BoundedData, copies it and returns
-// it.
-struct BoundedData *bounded_data_duplicate(struct BoundedData *bounded_data) {
-  struct BoundedData *new_bounded_data = malloc(sizeof(struct BoundedData));
-  if (new_bounded_data == NULL) {
-    perror("bounded_data_duplicate malloc");
-    abort();
-  }
-
-  new_bounded_data->size = bounded_data->size;
-  new_bounded_data->data = malloc(bounded_data->size);
-  if (new_bounded_data->data == NULL) {
-    perror("bounded_data_duplicate malloc 2");
-    free(new_bounded_data); // We free it just in case.
-    abort();
-  }
-
-  memcpy(new_bounded_data->data, bounded_data->data, bounded_data->size);
-
-  return new_bounded_data;
-}
-
 // De-allocates memory for the given BoundedData instance.
 void bounded_data_destroy(struct BoundedData *bounded_data) {
   if (bounded_data->data != NULL) {
@@ -49,12 +27,6 @@ void bounded_data_destroy(struct BoundedData *bounded_data) {
 // Prints the given BoundedData instance to standard output, no newline.
 void bounded_data_print(struct BoundedData *bounded_data) {
   printf("%s", bounded_data->data);
-}
-
-// true if the contents of the given BoundedData instance are representable as
-// text, false otherwise.
-bool bounded_data_is_text_representable(struct BoundedData *bounded_data) {
-  return is_text_representable(bounded_data->data, bounded_data->size);
 }
 
 #define FNV_OFFSET 14695981039346656037UL
--- src/bounded_data.h
+++ src/bounded_data.h
@@ -14,19 +14,11 @@ struct BoundedData {
 // otherwise.
 bool bounded_data_equals(struct BoundedData *a, struct BoundedData *b);
 
-// Allocates memory for a copy of the given BoundedData, copies it and returns
-// it.
-struct BoundedData *bounded_data_duplicate(struct BoundedData *bounded_data);
-
 // De-allocates memory for the given BoundedData instance.
 void bounded_data_destroy(struct BoundedData *bounded_data);
 
 // Prints the given BoundedData instance to standard output, no newline.
 void bounded_data_print(struct BoundedData *bounded_data);
-
-// true if the contents of the given BoundedData instance are representable as
-// text, false otherwise.
-bool bounded_data_is_text_representable(struct BoundedData *bounded_data);
 
 // Returns the 64-bit FNV-1a hash for the given data.
 // More information: https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
